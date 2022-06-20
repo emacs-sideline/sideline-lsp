d55169dd167607125a4afc01b4148e4a6668485a
Jen-Chieh Shen <jcs090218@gmail.com>
2022-06-20 17:04:28 +0800

    add src

---
+++ sideline-lsp.el
@@ -0,0 +1,37 @@
+;;; sideline-lsp.el --- Show lsp information with sideline  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Shen, Jen-Chieh
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; URL: https://github.com/jcs-elpa/sideline-lsp
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; Keywords: sideline lsp
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Show lsp information with sideline.
+;;
+
+;;; Code:
+
+
+
+(provide 'sideline-lsp)
+;;; sideline-lsp.el ends here
