59e85974add644c960e607b79915fe1997f11486
Jen-Chieh Shen <jcs090218@gmail.com>
2022-06-26 20:36:29 +0800

    keywords

--- sideline-lsp.el
+++ sideline-lsp.el
@@ -7,7 +7,7 @@
 ;; URL: https://github.com/emacs-sideline/sideline-lsp
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "27.1") (sideline "0.1.0") (lsp-mode "6.0") (dash "2.18.0") (ht "2.4") (s "1.12.0"))
-;; Keywords: sideline lsp
+;; Keywords: convenience lsp
 
 ;; This file is not part of GNU Emacs.
 
